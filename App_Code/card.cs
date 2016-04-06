6ae716fef91bc41435bdca3592226c8daa8c9613
Nickelis Jarvis <hfxdesign@gmail.com>
2016-04-05 20:33:58 -0400

    Refactor

--- App_Code/card.cs
+++ App_Code/card.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 public class Card
 {
     public enum color { BLACK, WHITE };
-
     public color cardColor { get; set; }
     public int ID { get; set; }
     public String text { get; set; }
-    public Card(int ID, color cardColor, String text)
-    {
+
+    public Card(int ID, color cardColor, String text) {
         this.ID = ID;
         this.cardColor = cardColor;
         this.text = text;
