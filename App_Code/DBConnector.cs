b74a3586dcce08ccf62fb519931d1d667f3a34a0
Nickelis Jarvis <hfxdesign@gmail.com>
2016-05-22 17:57:06 -0400

    Push to server

---
+++ App_Code/DBConnector.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+class DBConnect {
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+
+    public DBConnect() {
+        Initialize();
+    }
+
+    private void Initialize() {
+        server      = "devxt.com";
+        database    = "autocore_cards";
+        uid         = "autocore_cards";
+        password    = "C4rdz!";
+        string connectionString = 
+            "SERVER="   + server    + ";" + 
+            "DATABASE=" + database  + ";" +
+            "UID="      + uid       + ";" +
+            "PASSWORD=" + password  + ";";
+
+        connection = new MySqlConnection(connectionString);
+    }
+
+    private bool OpenConnection() {
+        try {
+            connection.Open();
+            return true;
+        } catch (MySqlException ex) {
+            switch (ex.Number) {
+                case 0:
+                    Console.WriteLine("Cannot connect to server.  Contact administrator");
+                    break;
+
+                case 1045:
+                    Console.WriteLine("Invalid username/password, please try again");
+                    break;
+            }
+            return false;
+        }
+    }
+
+    private bool CloseConnection() {
+        try {
+            connection.Close();
+            return true;
+        } catch (MySqlException ex) {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public LinkedList<WhiteCard> getWhiteDeck() {
+        string query = "SELECT * FROM whiteCards"
+    }
+}
--- App_Code/Deck.cs
+++ App_Code/Deck.cs
@@ -7,6 +7,43 @@ public class Deck<T> where T : WhiteCard {
     private List<bool> drawn;
 
     /// <summary>
+    /// Construct a new <see cref="Deck{T}"/> from another <see cref="System.Collections"/> type
+    /// </summary>
+    /// <param name="cardTable">Name of other <see cref="System.Collections"/></param> type
+    public Deck(T[] cardTable) {
+        for (int i = 0; i < cardTable.Length; i++) {
+            cards.Add(cardTable[i]);
+            drawn.Add(false);
+        }
+        remainingCards = cards.Count;
+    }
+
+    /// <summary>
+    /// Construct a new <see cref="Deck{T}"/> from a plain <see cref="Array"/>
+    /// </summary>
+    /// <param name="cardTable">Name of the <see cref="Array"/> to copy</param>
+    /// <param name="tableSize">size of the <see cref="Array"/ to copy></param>
+    public Deck(T[] cardTable, int tableSize) {
+        for (int i = 0; i < tableSize - 1; i++) {
+            cards[i] = cardTable[i];
+            drawn[i] = false;
+        }
+        remainingCards = cards.Count;
+    }
+
+    /// <summary>
+    /// Create a copy of another <see cref="Deck{T}"/>
+    /// </summary>
+    /// <param name="oldDeck">Name of the other <see cref="Deck{T}"/></param>
+    public Deck(Deck<T> oldDeck) {
+        for (int i = 0; i < oldDeck.cards.Count; i++) {
+            cards.Add(oldDeck.cards[i]);
+            drawn.Add(oldDeck.drawn[i]);
+        }
+        remainingCards = cards.Count;
+    }
+
+    /// <summary>
     /// Clean the deck up.
     /// This will create a new instance of the deck minus the drawn cards.
     /// This should be executed between rounds.
@@ -48,42 +85,5 @@ public class Deck<T> where T : WhiteCard {
         drawn[i] = true;
         --remainingCards;
         return tempCard;
-    }
-
-    /// <summary>
-    /// Construct a new <see cref="Deck{T}"/> from another <see cref="System.Collections"/> type
-    /// </summary>
-    /// <param name="cardTable">Name of other <see cref="System.Collections"/></param> type
-    public Deck(T[] cardTable) {
-        for (int i = 0; i < cardTable.Length; i++) {
-            cards.Add(cardTable[i]);
-            drawn.Add(false);
-        }
-        remainingCards = cards.Count;
-    }
-
-    /// <summary>
-    /// Construct a new <see cref="Deck{T}"/> from a plain <see cref="Array"/>
-    /// </summary>
-    /// <param name="cardTable">Name of the <see cref="Array"/> to copy</param>
-    /// <param name="tableSize">size of the <see cref="Array"/ to copy></param>
-    public Deck(T[] cardTable, int tableSize) {
-        for (int i = 0; i < tableSize - 1; i++) {
-            cards[i] = cardTable[i];
-            drawn[i] = false;
-        }
-        remainingCards = cards.Count;
-    }
-
-    /// <summary>
-    /// Create a copy of another <see cref="Deck{T}"/>
-    /// </summary>
-    /// <param name="oldDeck">Name of the other <see cref="Deck{T}"/></param>
-    public Deck(Deck<T> oldDeck) {
-        for (int i = 0; i < oldDeck.cards.Count; i++) {
-            cards.Add(oldDeck.cards[i]);
-            drawn.Add(oldDeck.drawn[i]);
-        }
-        remainingCards = cards.Count;
     }
 }
